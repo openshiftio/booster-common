48b913bee35aca1c5505335a8543bd07cedb082a
Chris Laprun <metacosm@gmail.com>
2017-09-28 10:49:04 +0200

    Add support for qualifier in next version computing.

--- release-script/booster-release.sh
+++ release-script/booster-release.sh
@@ -17,8 +17,13 @@ echo -e "${BLUE}CURRENT VERSION: ${YELLOW} ${CURRENT_VERSION} ${NC}"
 if [[ "$CURRENT_VERSION" == *-SNAPSHOT ]]
 then
     L=${#CURRENT_VERSION}
-    IDX=$(($L - 9))
-    NEW_VERSION=${CURRENT_VERSION:0:${IDX}}
+    PART=(${CURRENT_VERSION//-/ })
+    NEW_VERSION=${PART[0]}
+    QUALIFIER=${PART[1]}
+    if [[ "$QUALIFIER" != SNAPSHOT ]]
+    then
+        QUALIFIER="${QUALIFIER}-SNAPSHOT"
+    fi
 else
     echo -e "${RED} The current version (${CURRENT_VERSION}) is not a SNAPSHOT ${NC}"
     exit 1
@@ -37,7 +42,7 @@ TAG="v${NEW_VERSION}"
 echo -e "${BLUE}Creating the tag ${YELLOW}${TAG}${NC}"
 git tag -a ${TAG} -m "Releasing ${TAG}"
 
-NEXT_VERSION="$(($NEW_VERSION +1))-SNAPSHOT"
+NEXT_VERSION="$(($NEW_VERSION +1))-${QUALIFIER}"
 echo -e "${BLUE}Updating project version to: ${YELLOW}${NEXT_VERSION}${NC}"
 mvn versions:set -DnewVersion=${NEXT_VERSION} > bump-version-dev.log
 
